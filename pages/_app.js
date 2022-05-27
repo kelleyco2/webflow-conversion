649fc4914b554d74f04c1f8f4e8a34b1272c8bfe
Cooper Kelley <kelleyco2@gmail.com>
2022-05-27 10:19:52 -0600

    import webflow project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import "../styles/redd-summit.webflow.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
